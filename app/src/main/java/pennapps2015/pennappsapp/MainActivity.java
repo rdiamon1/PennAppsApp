8fe0be8ab7e7d64aca25bb502ed8fbf6e1ec9f5f
Ishita Date <ishitadate@gmail.com>
2015-09-05 19:03:31 -0400

    location services

--- app/src/main/java/pennapps2015/pennappsapp/MainActivity.java
+++ app/src/main/java/pennapps2015/pennappsapp/MainActivity.java
@@ -1,12 +1,20 @@
 package pennapps2015.pennappsapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         return true;
+
     }
 
     @Override
@@ -51,6 +60,53 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+
+        LocationManager locationManager = (LocationManager) this
+                .getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+
+            @Override
+            public void onLocationChanged(Location location) {
+                //call fct.
+                Log.d("Tag", "in onLocationChanged");
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+                Toast.makeText(MainActivity.this,
+                        "Provider Enabled: " + provider, Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Toast.makeText(MainActivity.this,
+                        "Provider Disabled: " + provider, Toast.LENGTH_SHORT)
+                        .show();
+            }
+        };
+        long minTime = 2 * 1000; //minimum time interval for update in seconds, i.e. 2 seconds
+        long minDistance = 1; //minimum distance interval for update in meters, i.e. 1 meters
+
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    public void requestPermissions(@NonNull String[] permissions, int requestCode)
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return true;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, locationListener);
+
+        return true;
+
     }
-}
+    }
