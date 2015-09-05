b734146ba89199fc8cf2d90cb30c8063fd2f04a8
rdiamon1 <rdiamon1@swarthmore.edu>
2015-09-05 15:17:56 -0400

    added drivingText activity

---
+++ app/src/main/java/pennapps2015/pennappsapp/DrivingText.java
@@ -0,0 +1,18 @@
+package pennapps2015.pennappsapp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class DrivingText extends Service {
+    public DrivingText() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO:
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+}
